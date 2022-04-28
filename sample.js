0b51de26d10ba48526cf47724e55dfb770ae1a71
MuhammedSmani <muhammedsmani11@gmail.com>
2022-04-28 18:48:40 +0200

    main

---
+++ sample.js
@@ -0,0 +1,87 @@
+let f = 10;
+
+if(true){
+    console.log(f);
+}
+
+
+
+
+
+var a = 10;
+var a = 20;
+var a = 30;
+var a = 40;
+
+console.log(a);
+
+
+
+
+
+console.log(4 + 4);
+console.log("4" + 4);
+console.log(4 - 4);
+console.log("4" - 4);
+console.log(4 == "4");
+console.log(4 === "4");
+console.log((4 + 2) * 5);
+
+
+
+
+
+const v = "test";
+
+const n = "test";
+
+console.log(null === "null");
+
+
+
+
+
+let k = true;
+
+console.log(k === "ture");
+
+
+
+
+
+let l = 5;
+
+if (l !== 2) {
+    console.log(l);
+} else if (l == 5) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+
+
+
+
+
+if (l !== 5) {
+    console.log(l);
+} else if (l == 5) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+
+
+
+
+if (l !== 5) {
+    console.log(l);
+} else if (l == 2) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+git mainn
