071f9b463ecd7f0f14326f1a57ad131f41d52562
MuhammedSmani <muhammedsmani11@gmail.com>
2022-04-28 18:26:17 +0200

    28

---
+++ index.js
@@ -0,0 +1,84 @@
+
+
+// var a = 2
+/*let b = 3;
+const c = 4;
+
+let f = 5
+
+ if(true){
+    console.log(f);
+}
+
+console.log(f);
+
+let d; // defined with undifined value
+
+console.log(d); */
+
+// var a = 2;
+// var a = 3;
+// var a = 4;
+// var a = 5;
+
+// console.log(a);
+
+
+
+// console.log("2" - 2);
+
+// + - * ( ) / = 
+
+// console.log(2 == "2");
+// console.log(2 === "2");
+
+
+
+// console.log((2 + 2) * 2);
+
+/* const a = "test";
+
+const b = "test";
+
+console.log(null === "null");
+ */
+
+
+
+/* let c = true;
+
+console.log(c === "true");  */
+
+
+// let testmukki = "test";
+
+// let testMukki = "test1"; RIGH WAY
+
+let a = 3;
+
+if(a !== 2){
+    console.log(a);
+} else if (a == 3) {
+    console.log("sakt");
+}
+else{
+    console.log("gabim");
+}
+
+if(a !== 3){
+    console.log(a);
+} else if (a == 3) {
+    console.log("sakt");
+}
+else{
+    console.log("gabim");
+}
+
+if(a !== 3){
+    console.log(a);
+} else if (a == 2) {
+    console.log("sakt");
+}
+else{
+    console.log("gabim");
+}
