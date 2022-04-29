4163adb944a5e989454167f9a4912cf0c38e5655
MuhammedSmani <muhammedsmani11@gmail.com>
2022-04-29 18:45:07 +0200

    mukki

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 // let a = 3;
 // let b = 5;
 // let c = 13;
-// let d = 59;
+// // let d = 59;
 
-// function mbledhja(num1,num2){
-//     return num1 + num2;
+// // function mbledhja(num1,num2){
+// //     return num1 + num2;
+// // }
+
+// // let shuma1 = mbledhja(a,b); 
+// // console.log(shuma1)
+
+// // let shuma2 = mbledhja(c,d); 
+// // console.log(shuma2);
+
+
+// let a = 3;
+// let b = 5;
+// let c = 13;
+
+// function kikiriki(kiki,riki,kikiriki){
+//     return kiki+riki+kikiriki;
 // }
 
-// let shuma1 = mbledhja(a,b); 
-// console.log(shuma1)
-
-// let shuma2 = mbledhja(c,d); 
-// console.log(shuma2);
+// console.log(kikiriki(a,b,c));
 
 
-let a = 3;
-let b = 5;
-let c = 13;
 
-function kikiriki(kiki,riki,kikiriki){
-    return kiki+riki+kikiriki;
-}
 
-console.log(kikiriki(a,b,c));
+let lista = ['otorinolaringolog', 7, ()=>{}, [2, '5']];
 
-()=>{}
+// console.log(lista);
+
+// console.log(lista[3][1]);
+
+let student1={id:1,emri:"Muhammed",mbiemri:"Smani"};
+let student2={id:2,emri:"Nuri",mbiemri:"Xhezairi"};
+
+// console.log(student1.emri);
+
+ let students = [
+     {id:1,emri:"Muhammed",mbiemri:"Smani"}, //0
+     {id:2,emri:"Nuri",mbiemri:"Xhezairi"} // 1
+    ];
+
+console.log(students[0].mbiemri);
